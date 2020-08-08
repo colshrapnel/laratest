4049b70ea368894599d5138c4e4cafc256d0dfe4
colshrapnel <col.shrapnel@gmail.com>
2020-08-08 15:25:25 +0300

    Add validation

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Client;
-use App\ClientPhone;
-use App\ClientEmail;
-
+use Illuminate\Http\Response;
 
 class ClientController extends Controller
 {
@@ -38,8 +36,24 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        $client = new Client();
-        $client->store($request);
+        $rules = [
+            'firstname' => 'required|max:255',
+            'lastname' => 'required|max:255',
+            'emails' => 'array|required',
+            'emails.*' => 'email|required',
+            'phones' => 'array|required',
+            'phones.*' => 'regex:/^\+\d+\(\d+\)[\d-]+$/',
+        ];
+        $validator = \Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $response = ['errors' => $validator->messages()];
+            return response()->json($response,Response::HTTP_BAD_REQUEST);
+        }else {
+            $client = new Client();
+            $client->store($request);
+            $response = ['id' => $client->id];
+            return response()->json($response);
+        }
     }
 
     /**
